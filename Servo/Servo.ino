a566d4d91e8e336ff2f159416adf33259e97e933
rhdtl78 <rhdtl78@gmail.com>
2017-05-20 02:29:34 +0900

    OfflinMode 추가

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -96,8 +96,7 @@ void webpage(){
 void IsConnected(){
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
-    while(1) ;
+    Serial.println("OfflineMode");
    }
 }
 
