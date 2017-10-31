5e3cf5d6243abb1e33a5bd4fa12694ebdb5b8dee
권성연 <02_458@naver.com>
2017-10-31 12:21:07 +0900

    CDS 수정. 상태를 Down, Close를 기준으로 TT TF FF로 나누었습니다. FT는 없다는 게 전제 조건이 됩니다. 그리고 isDown의 경우 가장 아래(295)일 경우에만 true로 설정합니다.

--- BlueToothControl/BlueToothControler/BlueToothControler.ino
+++ BlueToothControl/BlueToothControler/BlueToothControler.ino
@@ -11,8 +11,8 @@ int pin_dcMotor2 = 10;
 int pin_cds = A0;
 int angle = 0;
 int dcPower = 0;
-bool isDown = true;
-bool isClosed = true;
+bool isDown = true; //전부 내려와 있을 경우에만 true. 중간이나 위는 false입니다.
+bool isClosed = true; 
 bool isModifyMode = false;
 int second = 1000;
 //stop 20171030_ksy
@@ -39,33 +39,33 @@ void setup() {
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  //CDS();
-   MotorWrite();
-  delay(1000);
-//  Serial.println(stat_move);
-//  Serial.println(stat_time);
-/*  if(stat_stop==0){
-  	stat_time += stat_move;
-  	if(stat_time < 5){
-		  stat_stop = -1;
-      isDown = false;
-	  }
-	  else if(stat_time > 295){
-		  stat_stop = 1;
-      isDown = true;
-	  }
-	  else{
-		 
-		  changeDC();
-	  }
-  }
-  else{
-  	if(stat_stop != stat_move){
-		stat_stop = 0;
+	CDS();
+	MotorWrite();
+	delay(1000);
+	Serial.println(stat_move);
+	Serial.println(stat_time);
+	if(stat_stop==0){
+		//안 멈춰있는 상태.
 		stat_time += stat_move;
+		if(stat_time < 5){
+			stat_stop = -1;
+		}
+		else if(stat_time > 295){
+			//다 내려왔으므로 isDown을 true로 변경
+			stat_stop = 1;
+			isDown = true;
+		}
+		else{
+			changeDC();
+		}
 	}
-  }
-  */
+	else{
+		//멈춤 상태.
+		if(stat_stop != stat_move){ //방향이 바뀔 경우
+			stat_stop = 0;
+			stat_time += stat_move;
+		}
+	}
 }
 
 /*
@@ -123,7 +123,6 @@ void MotorWrite() {
 
 /*
 	DC모터만 따로 움직이게끔 설정.
-	
 */
 void changeDC(){
 	if(stat_move == 1){
@@ -141,49 +140,48 @@ void changeDC(){
 }
 
 void CDS(){
-  if(isModifyMode == false){
-    int cds_val = analogRead(pin_cds);
-    if(0 <= cds_val && cds_val <500){
-      //내리고 닫아야함.
-      
-      Serial.println("Down,Close");
-      if(isDown == false){
-      	stat_move = 1;
-      }
-      if(isClosed == false){
-        m_servo.attach(pin_servo);
-        m_servo.write(3600);
-        m_servo.detach();
-        isClosed = true;
-      }
-    }else if(500 <= cds_val && cds_val < 700){
-      //열기만 함
-      Serial.println("Down,Open");
-      if(isClosed == true){
-        m_servo.attach(pin_servo);
-        m_servo.write(-3600);
-        m_servo.detach();
-        isClosed = false;
-      }
-      if(isClosed == false){
-      	stat_move = 1;
-      }
-    }else if(700 <= cds_val){
-      //열고 올려야함
-      Serial.println("Up,Open");
-      if(isClosed == true){
-        m_servo.attach(pin_servo);
-        m_servo.write(-3600);
-        m_servo.detach();
-        isClosed = false;
-      }
-      if(isClosed == false){
-        Serial.println("isDown is true");
-      	stat_move = -1;
-        
-      }
-    }
-  }
+	if(isModifyMode == false){
+		int cds_val = analogRead(pin_cds);
+		if(0 <= cds_val && cds_val <500){
+			//내리고 닫아야함.(TT)
+			Serial.println("Down,Close");
+			if(isDown == false){
+				stat_move = 1;
+			}
+			else if(isClosed == false){
+				m_servo.attach(pin_servo);
+				m_servo.write(3600);
+				m_servo.detach();
+				isClosed = true;
+			}
+		}else if(500 <= cds_val && cds_val < 700){
+			//열기만 함(TF)
+			Serial.println("Down,Open");
+			if(isDown == false){
+				stat_move = 1;
+			}
+			else if(isClosed == true){
+				m_servo.attach(pin_servo);
+				m_servo.write(-3600);
+				m_servo.detach();
+				isClosed = false;
+			}
+			
+		}else if(700 <= cds_val){
+			//열고 올려야함(FF)
+			Serial.println("Up,Open");
+			if(isClosed == true){
+				m_servo.attach(pin_servo);
+				m_servo.write(-3600);
+				m_servo.detach();
+				isClosed = false;
+			}
+			else{
+				stat_move = -1;
+				isDown = false; //제일 Down인 상태가 아니므로
+			}
+		}
+	}
 }
 
 
