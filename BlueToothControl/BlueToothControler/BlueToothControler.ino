69da6682d003eb4e5f5c724832818bf8cdb9291a
JunghoonKong <rhdtl78@gmail.com>
2017-11-02 15:48:36 +0900

    모드 추가 및 코드 최적화
    사용자 명령과 자동 상태의 충돌 현상을 해결
    불필요한 코드의 반복을 제거
    상수 변수의 상수화.
    의미에 맞는 변수명/ 함수명 사용.

--- BlueToothControl/BlueToothControler/BlueToothControler.ino
+++ BlueToothControl/BlueToothControler/BlueToothControler.ino
@@ -2,24 +2,23 @@
 #include <SoftwareSerial.h>
 #include "Servo.h"
 
-int pin_blueTx = 2;
-int pin_blueRx = 3;
-int pin_servo = 5;
-int pin_icEnable = 11;
-int pin_dcMotor1 = 9;
-int pin_dcMotor2 = 10;
-int pin_cds = A0;
-int angle = 0;
-int dcPower = 0;
+const int pin_blueTx = 2;
+const int pin_blueRx = 3;
+const int pin_servo = 5;
+const int pin_icEnable = 11;
+const int pin_dcMotor1 = 9;
+const int pin_dcMotor2 = 10;
+const int pin_cds = A0;
+const int dcPower = 255;
 bool isDown = true; //전부 내려와 있을 경우에만 true. 중간이나 위는 false입니다.
 bool isClosed = true; 
-bool isModifyMode = false;
-int second = 1000;
+bool isManualMode = false;
 //stop 20171030_ksy
-int stat_move = 0;
-int stat_stop = 1;
-int stat_time= 295; // 5~295
-
+int stat_move = 2;
+int stat_stop = 2;
+int stat_time= 285; // 5~285
+const int time_limit = 285;
+String tmp;
 SoftwareSerial mySerial(pin_blueTx,pin_blueRx);
 Servo m_servo;
 
@@ -30,93 +29,54 @@ void setup() {
   pinMode(pin_icEnable, OUTPUT);
   pinMode(pin_dcMotor1, OUTPUT);
   pinMode(pin_dcMotor2, OUTPUT);
-  m_servo.attach(pin_servo);
-  m_servo.write(90);
-  delay(1000);
-  m_servo.detach();
-  delay(500);
+  delay(500); 
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-	CDS();
-	MotorWrite();
+  if(mySerial.available()){
+    tmp = mySerial.readString();
+    Serial.println(tmp);
+    if(tmp.equals("Manual")){
+      isManualMode = true;
+    }else if(tmp.equals("Auto")){
+      isManualMode = false;
+    }
+  }
+  
+  if(isManualMode){
+    Bluetooth();
+  }
+  else{
+    CDS();
+  }
+  CheckState();
 	delay(1000);
-	Serial.println(stat_move);
-	Serial.println(stat_time);
-	if(stat_stop==0){
-		//안 멈춰있는 상태.
-		stat_time += stat_move;
-		if(stat_time < 5){
-			stat_stop = -1;
-		}
-		else if(stat_time > 295){
-			//다 내려왔으므로 isDown을 true로 변경
-			stat_stop = 1;
-			isDown = true;
-		}
-		else{
-			changeDC();
-		}
-	}
-	else{
-		//멈춤 상태.
-		if(stat_stop != stat_move){ //방향이 바뀔 경우
-			stat_stop = 0;
-			stat_time += stat_move;
-		}
-	}
 }
 
 /*
 블루투스를 이용, 어플로 각도 조절, DC모터 조절
-DC : -255 ~ 255
-angle : 0 ~ 90
 */
 
-void MotorWrite() {
-
-  if (mySerial.available()) {
-    String tmp = mySerial.readString();
-    Serial.println(tmp);
-    if (tmp.equals("Backward") || tmp.equals("Forward")) {
-      m_servo.attach(pin_servo, 1000, 5000);
-      if(tmp.equals("Backward")){
-        angle = 0;
-      }else if(tmp.equals("Forward")){
-        angle = 180;
-      }
-      m_servo.write(angle);
-      delay(4500);
-      m_servo.detach();
-      Serial.print("Angle : ");
-      Serial.println(angle);
+void Bluetooth() {
+  if (tmp.equals("Backward") || tmp.equals("Forward")) {
+    if(tmp.equals("Backward") && !isClosed){
+      ServoWrite(0);
+      isClosed = true;
+    }else if(tmp.equals("Forward") && isClosed){
+      ServoWrite(3600);
+      isClosed = false;
     }
-    else if (tmp.equals("Open") || tmp.equals("Close") || tmp.equals("Stop")) {
-      dcPower = 255;
-      if (tmp.equals("Open")) {
-        int speed = (-1)*dcPower;
-        analogWrite(pin_icEnable, speed);
-        digitalWrite(pin_dcMotor1, true);
-        digitalWrite(pin_dcMotor2, false);
-        Serial.print("Speed : ");
-        Serial.println(speed);
-      }
-      else if (tmp.equals("Close")) {
-        int speed = dcPower;
-        analogWrite(pin_icEnable, speed);
-        digitalWrite(pin_dcMotor1, false);
-        digitalWrite(pin_dcMotor2, true);
-        Serial.print("Speed : ");
-        Serial.println(speed);
-      }else if(tmp.equals("Stop")){
-        int speed = 0;
-        analogWrite(pin_icEnable, speed);
-        digitalWrite(pin_dcMotor1, false);
-        digitalWrite(pin_dcMotor2, false);
-        Serial.print("Speed : ");
-        Serial.println(speed);
-      }
+  }
+  else if (tmp.equals("Open") || tmp.equals("Close") || tmp.equals("Stop")) {
+    if (tmp.equals("Open")) {
+      stat_move = -1;
+    }
+    else if (tmp.equals("Close")) {
+      stat_move = 1;      
+    }else{
+      stat_stop = 2;
+      stat_move = 2;
     }
   }
 }
@@ -125,63 +85,89 @@ void MotorWrite() {
 	DC모터만 따로 움직이게끔 설정.
 */
 void changeDC(){
-	if(stat_move == 1){
-		int speed = 255;
-		analogWrite(pin_icEnable, (-1)*speed);
-		digitalWrite(pin_dcMotor1, true);
-		digitalWrite(pin_dcMotor2, false);
-	}
-	else if(stat_move == -1){
-		int speed = 255;
-		analogWrite(pin_icEnable, speed);
-		digitalWrite(pin_dcMotor1, false);
-		digitalWrite(pin_dcMotor2, true);
-	}
+  if(stat_stop != 0){
+    digitalWrite(pin_dcMotor1, false);
+    digitalWrite(pin_dcMotor2, false);
+  }
+  else{
+    analogWrite(pin_icEnable, (-1)*stat_move*dcPower);
+    if(stat_move == 1){
+      digitalWrite(pin_dcMotor1, true);
+      digitalWrite(pin_dcMotor2, false);
+    }
+    else if(stat_move == -1){
+      digitalWrite(pin_dcMotor1, false);
+      digitalWrite(pin_dcMotor2, true);
+    }
+  } 
 }
 
 void CDS(){
-	if(isModifyMode == false){
-		int cds_val = analogRead(pin_cds);
-		if(0 <= cds_val && cds_val <500){
-			//내리고 닫아야함.(TT)
-			Serial.println("Down,Close");
-			if(isDown == false){
-				stat_move = 1;
-			}
-			else if(isClosed == false){
-				m_servo.attach(pin_servo);
-				m_servo.write(3600);
-				m_servo.detach();
-				isClosed = true;
-			}
-		}else if(500 <= cds_val && cds_val < 700){
-			//열기만 함(TF)
-			Serial.println("Down,Open");
-			if(isDown == false){
-				stat_move = 1;
-			}
-			else if(isClosed == true){
-				m_servo.attach(pin_servo);
-				m_servo.write(-3600);
-				m_servo.detach();
-				isClosed = false;
-			}
-			
-		}else if(700 <= cds_val){
-			//열고 올려야함(FF)
-			Serial.println("Up,Open");
-			if(isClosed == true){
-				m_servo.attach(pin_servo);
-				m_servo.write(-3600);
-				m_servo.detach();
-				isClosed = false;
-			}
-			else{
-				stat_move = -1;
-				isDown = false; //제일 Down인 상태가 아니므로
-			}
-		}
-	}
+  int cds_val = analogRead(pin_cds);
+  if(cds_val <500){
+    //내리고 닫아야함.(TT)
+    Serial.println("Down,Close");
+    if(isDown == false){
+      stat_move = 1;
+    }
+    else if(isClosed == false){
+      ServoWrite(0);
+      isClosed = true;
+    }
+  }else if(cds_val < 700){
+    //열기만 함(TF)
+    Serial.println("Down,Open");
+    if(isDown == false){
+      stat_move = 1;
+    }
+    else if(isClosed == true){
+      ServoWrite(3600);
+      isClosed = false;
+    }
+    
+  }else{
+    //열고 올려야함(FF)
+    Serial.println("Up,Open");
+    if(isClosed == true){
+      ServoWrite(3600);
+      isClosed = false;
+    }
+    else{
+      stat_move = -1;
+      isDown = false; //제일 Down인 상태가 아니므로
+    }
+  }
 }
 
+void ServoWrite(int angle){
+  m_servo.attach(pin_servo, 1000, 3000);
+  m_servo.write(angle);
+  delay(2500);
+  m_servo.detach();
+  Serial.print("Angle : ");
+  Serial.println(angle);
+}
+
+void CheckState(){
+  Serial.println(stat_move);
+  Serial.println(stat_time);
+  if(stat_stop==0){
+    //안 멈춰있는 상태.
+    stat_time += stat_move;
+    if(stat_time < 5){
+      stat_stop = -1;
+    }
+    else if(stat_time >= time_limit){
+      //다 내려왔으므로 isDown을 true로 변경
+      stat_stop = 1;
+      isDown = true;
+    }
+  }
+  else if(stat_stop != stat_move){
+    //멈춤 상태에서 방향이 바뀔 경우
+      stat_stop = 0;
+      stat_time += stat_move;
+  }
+  changeDC();
+}
 
